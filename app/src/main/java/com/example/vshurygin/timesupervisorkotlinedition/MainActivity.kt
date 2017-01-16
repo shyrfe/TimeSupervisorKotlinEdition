b287b519734757899531b5f69203a96be1b81024
vshurygin <shurvd@gmail.com>
2017-01-16 14:54:08 +0300

    convert viewList and add, clear, calculate function to project

--- app/src/main/java/com/example/vshurygin/timesupervisorkotlinedition/MainActivity.kt
+++ app/src/main/java/com/example/vshurygin/timesupervisorkotlinedition/MainActivity.kt
@@ -3,17 +3,23 @@ package com.example.vshurygin.timesupervisorkotlinedition
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.IntegerRes
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.Toast
 import io.realm.Realm
 import com.example.vshurygin.timesupervisorkotlinedition.DateRecord
 import io.realm.RealmConfiguration
+import kotlinx.android.synthetic.main.activity_listview.view.*
 import kotlin.properties.Delegates
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.regex.Pattern
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,12 +35,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Realm.init(this);
-        val config = RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build()
-        realm = Realm.getInstance(config);
-        //realm = Realm.getDefaultInstance();
+        //val config = RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build()
+        //realm = Realm.getInstance(config);
+        realm = Realm.getDefaultInstance();
         //realm.deleteAll();
         CalculateOfTimeButton.setOnClickListener {
             calculateOfTimeButtonListener();
+        }
+        AddRecordButton.setOnClickListener {
+            addRecordButtonClickListener()
+        }
+        ClearAllRecordsButton.setOnClickListener {
+            deleteAllRecords();
         }
 
 
@@ -42,9 +54,15 @@ class MainActivity : AppCompatActivity() {
         val allRecords : MutableList<DateRecord> = mutableListOf();
         for(item:DateRecord in results){
             allRecords.add(item);
+            Log.d("add","add");
         }
-
-
+        for(item:DateRecord in allRecords){
+            Log.d("Result",item.TheBeginningOfDayTime.toString());
+            Log.d("Result",item.TheEndingOfDayTime.toString());
+            Log.d("Result",item.Date.toString());
+            Log.d("Result",item.WeekNumber.toString());
+        }
+        RecordsList.adapter = ResultListAdapter(allRecords);
     }
 
     override fun onDestroy() {
@@ -53,20 +71,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun addRecord(theBeginningOfTheDay: String, theEndingOfTheDay: String, date: String, weekNumber: Int){
-        realm.executeTransaction { Realm.Transaction {
-                fun execute(realm: Realm){
+        realm.executeTransaction {
                     var record = realm.createObject(DateRecord::class.java);
                     record.TheBeginningOfDayTime = theBeginningOfTheDay;
                     record.TheEndingOfDayTime = theEndingOfTheDay;
                     record.Date = date;
                     record.WeekNumber = weekNumber;
-                }
-            }
         }
         Log.d("addRecords","Records Added");
     }
+    fun deleteAllRecords(){
+        realm.executeTransaction { Realm.Transaction {
+                fun execute(realm:Realm){
+                    realm.deleteAll();
+                }
+            }
+        }
+    }
+
 
     fun calculateHoursForWeek(week:Int):Int{
+        var TotalHours = 0;
+        var TotalMinute = 0;
+        var TotalTime = 0;
+
         for(record:DateRecord in realm.where(DateRecord::class.java).findAll()){
             if(record.WeekNumber == week){
                 val sdf = SimpleDateFormat("HH:mm");
@@ -100,10 +128,73 @@ class MainActivity : AppCompatActivity() {
                     WorkHour++;
                 }
 
-                //TO BE CONTINUE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                if(WorkHour >= 0){
+                    if(TotalHours >= 0){
+                        if(TotalMinute + WorkMinute >= 60){
+                            TotalHours = TotalHours + (WorkHour + 1);
+                            TotalMinute = (TotalMinute + WorkMinute) - 60;
+                        }
+                        else{
+                            TotalHours = TotalHours + WorkHour;
+                            TotalMinute = TotalMinute + WorkMinute;
+                        }
+                    }
+                    else{
+                        if(Math.abs(TotalHours) < WorkHour){
+                            if(TotalMinute < WorkMinute){
+                                TotalHours = TotalHours + WorkHour;
+                                TotalMinute = WorkMinute - TotalMinute;
+                            }
+                            else{
+                                WorkHour--;
+                                TotalHours = TotalHours + WorkHour;
+                                TotalMinute = (60 + WorkMinute) - TotalMinute;
+                            }
+                        }
+                        else{
+                            if(TotalMinute >= WorkMinute){
+                                TotalHours = TotalHours + WorkHour;
+                                TotalMinute = TotalMinute - WorkMinute;
+                            }
+                            else{
+                                WorkHour++;
+                                TotalHours = TotalHours + WorkHour;
+                                TotalMinute = (60 + TotalMinute) - WorkMinute;
+                            }
+                        }
+                    }
+                }
+                else{
+                    if(TotalHours >= 0){
+                        if(TotalMinute - WorkMinute >= 0){
+                            TotalHours = TotalHours - Math.abs(WorkHour);
+                            TotalMinute = TotalMinute - WorkMinute;
+                        }
+                        else{
+                            TotalHours = TotalHours - (Math.abs(WorkHour)+1);
+                            TotalMinute = 60 + (TotalMinute - WorkMinute);
+                        }
+                    }
+                    else{
+                        if(TotalMinute + WorkMinute >= 60){
+                            TotalHours = TotalHours - (Math.abs(WorkHour)+1);
+                            TotalMinute = 60 - (TotalMinute + WorkMinute);
+                        }
+                        else{
+                            TotalHours = TotalHours - Math.abs(WorkHour);
+                            TotalMinute = TotalMinute + WorkMinute;
+                        }
+                    }
+                }
             }
         }
-       return 1;
+        if(TotalHours > 0){
+            TotalTime = (Math.abs(TotalHours)*100+Math.abs(TotalMinute));
+        }
+        else{
+            TotalTime = (Math.abs(TotalHours)*100+Math.abs(TotalMinute)) * -1;
+        }
+       return TotalTime;
     }
 
     fun calculateOfTimeButtonListener(){
@@ -121,6 +212,66 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText( mLocalContext,R.string.EmptyWeek,Toast.LENGTH_SHORT).show();
         }
     }
+    fun addRecordButtonClickListener(){
+        val beginningOfTheDay = TheBeginningOfTheDay.text.toString();
+        val endingOfTheDay = TheEndingOfTheDay.text.toString();
+        val date = TodaysDate.text.toString();
+        val sWeekNumber = WeekNumber.text.toString();
 
+        fun checkTime(value:String):Boolean{
+            val pat = Pattern.compile("^(([0,1][0-9])|(2[0-3])):[0-5][0-9]$");
+            val mat = pat.matcher(value);
+            return mat.matches();
+        }
+        fun checkDate(value:String):Boolean{
+            val pat = Pattern.compile("(0[1-9]|1[0-9]|2[0-9]|3[01])-(0[1-9]|1[012])-[0-9]{4}");
+            val mat = pat.matcher(value);
+            return mat.matches();
+        }
+
+        if((checkDate(date))&&(checkTime(beginningOfTheDay))&&(checkTime(endingOfTheDay))&&(sWeekNumber != "")){
+            val weeknumber = Integer.parseInt(sWeekNumber);
+            addRecord(beginningOfTheDay,endingOfTheDay,date,weeknumber);
+        }
+        else{
+            Log.d("Validate","Date: " + checkDate(date).toString());
+            Log.d("Validate","Beginning: " + checkTime(beginningOfTheDay).toString());
+            Log.d("Validate","Ending: " + checkTime(endingOfTheDay).toString());
+            Log.d("Validate","Input data are not valid");
+        }
+
+    }
 }
 
+class ResultListAdapter(records:List<DateRecord>) : BaseAdapter(){
+
+    val mRecords : List<DateRecord>;
+
+    init{
+        mRecords = records;
+    }
+
+    override fun getCount(): Int {
+        return mRecords.size;
+    }
+    override fun getItem(position: Int):DateRecord{
+        return mRecords.get(position);
+    }
+    override fun getItemId(id:Int):Long{
+        return id.toLong();
+    }
+    override fun getView(position: Int,convertView:View?,parent:ViewGroup):View?{
+        var view = convertView;
+        if(view == null){
+            view = LayoutInflater.from(parent.context).inflate(R.layout.activity_listview,parent,false);
+        }
+        val record = getItem(position);
+
+        view?.listItemWeekNumber?.setText(record.WeekNumber.toString());
+        view?.listItemBeginningOfDayTime?.setText(record.TheBeginningOfDayTime.toString());
+        view?.listItemEndingOfDayTime?.setText(record.TheEndingOfDayTime.toString());
+        view?.listItemDate?.setText(record.Date.toString());
+
+        return view;
+    }
+}
